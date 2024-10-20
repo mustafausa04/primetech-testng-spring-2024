6140b77a3a8c38442a54c9fc8bce146c1df89176
Mike Saleh <mikeusa04@msn.com>
2024-10-20 16:18:43 -0700

    my first commit

--- src/test/java/testcases/Calculator.java
+++ src/test/java/testcases/Calculator.java
@@ -1,2 +1,18 @@
-package testcases;public class Calculator {
+package testcases;
+
+
+import org.testng.annotations.Test;
+
+public class Calculator {
+
+    @Test
+    public  void calculatorTest (){
+        System.out.println("Hey");
+    }
+
+    @Test
+    public  void test2 (){
+        System.out.println("Hello");
+    }
+
 }
